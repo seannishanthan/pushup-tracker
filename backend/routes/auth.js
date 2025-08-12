7b80e81130dd109a81807a0db22bb9675a9ed9bb
Sean Nishanthan <seannishanthan@gmail.com>
2025-08-12 16:02:42 -0400

    Update jwt expiry to 1 hour to reduce time of attacks if jwt is leaked

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,7 +12,7 @@ const generateToken = (userId) => {
     return jwt.sign(
         { userId },
         process.env.JWT_SECRET,
-        { expiresIn: '7d' } // Token expires in 7 days
+        { expiresIn: '1h' } // Token expires in 1 hour
     );
 }
 
