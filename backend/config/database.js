799d4fdc753892ef7eab2531543318af0548f108
Sean Nishanthan <seannishanthan@gmail.com>
2025-09-13 01:58:09 -0400

    fixed maxbuffer entries error in mongoose

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 
 // Optimize mongoose connection settings for serverless/cold start scenarios
 mongoose.set('bufferCommands', false); // Disable mongoose buffering
-mongoose.set('bufferMaxEntries', 0); // Disable mongoose buffering
 
 const connectDB = async () => {
     try {
@@ -19,7 +18,6 @@ const connectDB = async () => {
             maxPoolSize: 10, // Maintain up to 10 socket connections
             serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
             socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-            bufferMaxEntries: 0, // Disable mongoose buffering
             bufferCommands: false, // Disable mongoose buffering
             // Connection pool settings
             maxIdleTimeMS: 30000, // Close connections after 30 seconds of inactivity
